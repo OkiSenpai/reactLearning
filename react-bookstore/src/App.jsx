c9e556bf7bfa21a1e9aff43c70385f8942217451
OkiSenpai <omersatelit@gmail.com>
2025-10-27 16:15:55 +0100

    exe2 lab 3 done

---
+++ react-bookstore/src/App.jsx
@@ -0,0 +1,19 @@
+
+import "./App.css";
+import Footer from "./Footer.jsx";
+import ClickCounter from "./ClickCounter.jsx";
+import Header from "./Headeer.jsx";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Header />
+        <ClickCounter />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
